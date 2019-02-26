918b7bdb27f90b86cf561c5bacff1626353ec7ee
Jose Carlos Diaz <josecarlos.diaz@josecarlos-ltm5.internal.salesforce.com>
2019-02-26 09:45:55 +0000

    Exercise 2-5 Few people

--- force-app/Exercises/main/default/aura/StudentBrowser/StudentBrowserHelper.js
+++ force-app/Exercises/main/default/aura/StudentBrowser/StudentBrowserHelper.js
@@ -1,5 +1,21 @@
 ({
-    helperMethod : function() {
+	onInit: function (component, event, helper) {
+		var students = [];
+		var studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', "Amit", "Simon"];
+		studentNames.forEach((studentName, index) => {
+			students.push(
+				{
+					'sobjectType': 'Contact',
+					'Name': studentName,
+					'PhotoUrl': '/services/images/photo/003B0FakePictId',
+					'Id': index
+				}
+			);
+		})
 
-    }
+		// TODO: Set component attribute here
+		component.set('v.studentList', students);
+
+	}
 })
+
