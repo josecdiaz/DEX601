134bb0d92137d810c3d15b09ef2bfa8bf41d8901
Jose Carlos Diaz <josecarlos.diaz@josecarlos-ltm5.internal.salesforce.com>
2019-02-26 12:36:43 +0000

    Exercise 2-8 Completed

--- force-app/Exercises/main/default/aura/StudentBrowserForm/StudentBrowserFormController.js
+++ force-app/Exercises/main/default/aura/StudentBrowserForm/StudentBrowserFormController.js
@@ -1,5 +1,22 @@
 ({
-    myAction : function(component, event, helper) {
+	doInit: function (component, event, helper) {
+		helper.onInit(component, event, helper);
+	},
+	onInstructorChange: function (component, event, helper) {
+		component.set('v.selectedDeliveryId', '');
+		if (component.get('v.selectedInstructorId') === '') {
+			component.set('v.deliveries', []);
+		} else {
+			helper.callServer(
+				component,
+				"c.getDeliveriesByInstructor",
+				(response) => {
+					component.set('v.deliveries', response);
+				}, {
+					instructorId: component.get('v.selectedInstructorId')
+				}
+			)
+		};
+	}
 
-    }
 })
