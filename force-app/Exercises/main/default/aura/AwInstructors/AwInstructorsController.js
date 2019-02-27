ae1d97e996ddf84e072b7b4131605574361bd63f
Jose Carlos Diaz <josecarlos.diaz@josecarlos-ltm5.internal.salesforce.com>
2019-02-27 14:06:02 +0000

    Exercise 5-1 Test Layout

--- force-app/Exercises/main/default/aura/AwInstructors/AwInstructorsController.js
+++ force-app/Exercises/main/default/aura/AwInstructors/AwInstructorsController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+	onNavSelect: function (component, event, helper) {
+		var selectionName = event.getParam('name');
+		if (selectionName == 'students') {
+			component.set('v.viewMode', 'students');
+		} else {
+			component.set('v.viewMode', 'certifications');
+		}
+	}
 })
---
+++ force-app/Exercises/main/default/aura/AwInstructorsNavigation/AwInstructorsNavigationController.js
@@ -0,0 +1,10 @@
+({
+    doInit: function(component, event, helper) {
+		helper.callServer(
+		  component,
+		  "c.getCertifications",
+		  function(response) {
+			 component.set('v.certifications', response);
+		  }
+); }
+})
