9b2780580fef8a9f951b162d972a03324823bd86
Jose Carlos Diaz <josecarlos.diaz@josecarlos-ltm5.internal.salesforce.com>
2019-02-28 13:33:59 +0000

    Exercise 6-2 Updates StudentDetail

--- force-app/Exercises/main/default/aura/DataGridTable/DataGridTableController.js
+++ force-app/Exercises/main/default/aura/DataGridTable/DataGridTableController.js
@@ -1,6 +1,13 @@
 ({
-	onRowClick : function(component, event, helper) {
+	onRowClick: function (component, event, helper) {
 		var target = event.currentTarget;
-		helper.setSelectedRow(component,target);
+		helper.setSelectedRow(component, target);
+
+		var compEvent = component.getEvent("rowclick");
+		compEvent.setParams({
+			pk: target.getAttribute('data-pk'),
+			domEl: target
+		});
+		compEvent.fire();
 	}
 })
--- force-app/Exercises/main/default/aura/StudentBrowser/StudentBrowserController.js
+++ force-app/Exercises/main/default/aura/StudentBrowser/StudentBrowserController.js
@@ -12,6 +12,10 @@
 		var contactId = event.getParam('contactId');
 		component.set('v.selectedContactId', contactId);
 		helper.broadcastStudentSelected(component);
+	},
+	onDataGridRowClick: function (component, event, helper) {
+		component.set('v.selectedContactId', event.getParam('pk'));
+		helper.broadcastStudentSelected(component);
 	}
 
 })
