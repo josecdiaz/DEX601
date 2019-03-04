f6443c076d6870b93f38e12f9595ce58a5b49c12
Jose Carlos Diaz <josecarlos.diaz@josecarlos-ltm5.internal.salesforce.com>
2019-03-04 11:35:16 +0000

    Delete Exercise folder

--- EXFiles/data/DeleteAllScratchOrgs.sh
+++
@@ -1,7 +0,0 @@
-echo "Please do not use this in a real production org, because you may lose work :-)"
-echo "Exporting scratch orgs..."
-sfdx force:data:soql:query -q "SELECT Id, Username FROM ScratchOrgInfo" -r csv -u dhDEX601  > ./ScratchOrgs.csv
-cat ./ScratchOrgs.csv
-echo "Deleting found scratch Orgs..."
-sfdx force:data:bulk:delete -f ./ScratchOrgs.csv -s ScratchOrgInfo -w 10 -u dhDEX601
-rm -f ./ScratchOrgs.csv
--- EXFiles/scripts/ETCopyData.sh
+++
@@ -1,7 +0,0 @@
-# Execute in Mac using: ./EXFiles/scripts/ETCopyData.sh
-echo "*** ETCopyData plugin"
-sfdx plugins:uninstall etcopydata
-sfdx plugins:install etcopydata
-echo "*** Updating plugins"
-sfdx plugins:update
-
